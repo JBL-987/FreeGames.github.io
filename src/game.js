d29644e96353ec3428e9b5d540ed9fc911b3c737
Loi <129076315+JBL-987@users.noreply.github.com>
2024-05-29 23:21:16 +0700

    Add files via upload

---
+++ src/game.js
@@ -0,0 +1,14 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('keydown', function() {
+        // Hide the start overlay
+        document.getElementById('start-overlay').style.display = 'none';
+        
+        // Start the game
+        startGame();
+    });
+
+    function startGame() {
+    }
+});
