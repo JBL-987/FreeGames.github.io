1aa3ab24998e9346642a52f513c6753596e155af
Loi <129076315+JBL-987@users.noreply.github.com>
2024-06-14 11:00:32 +0700

    Update game.js
    minified code

--- src/game.js
+++ src/game.js
@@ -1,14 +1,1 @@
-'use strict';
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('click', function() {
-        // Hide the start overlay
-        document.getElementById('start-overlay').style.display = 'none';
-        
-        // Start the game
-        startGame();
-    });
-
-    function startGame() {
-    }
-});
+'use strict';document.addEventListener('DOMContentLoaded',function(){document.addEventListener('click',function(){document.getElementById('start-overlay').style.display='none';startGame()});function startGame(){}})
